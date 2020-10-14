2b79ca6afc98747c361d25f77cd89ab69de9b1f0
Heather01 <heather.murray.2019@uni.strath.ac.uk>
2020-10-14 23:54:45 +0100

    completed question 5 of exercise 1.15

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-//        Circle circle1 = new Circle();
-//        Circle circle2 = new Circle(1.5);
+        Circle circle1 = new Circle();
+        System.out.println("This is the fist instance of the circle's radius " + circle1.getRadius() + " and colour " + circle1.getColour());
+
+        Circle circle2 = new Circle(1.5);
+        System.out.println("This is the fist instance of the circle's radius " + circle2.getRadius() + " and colour " + circle2.getColour());
+
 
         Scanner input = new Scanner(System.in);
+
         double inputRadius = input.nextDouble();
+        Circle circle3 = new Circle(inputRadius);
+        System.out.println("This is the fist instance of the circle's radius " + circle3.getRadius() + " and colour " + circle3.getColour());
+
+        double inputRadius2 = input.nextDouble();
         double inputDoubleColour = input.nextDouble();
+        Circle circle4 = new Circle(inputRadius2,inputDoubleColour);
+        System.out.println("This is the fist instance of the circle's radius " + circle4.getRadius() + " and colour " + circle4.getColour());
 
-//        Circle circle3 = new Circle(inputRadius);
-        Circle circle4 = new Circle(inputRadius,inputDoubleColour);
-
-        System.out.println( circle4.getColour());
 
 
 
