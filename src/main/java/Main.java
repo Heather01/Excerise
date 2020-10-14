15354a0bc1f6e63dc8459a10fb8d98a4ffdd2610
Heather01 <heather.murray.2019@uni.strath.ac.uk>
2020-10-14 23:33:40 +0100

    completed question 1 of exercise 1.15

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Circle circle1 = new Circle();
-        Circle circle2 = new Circle(1.5);
+//        Circle circle1 = new Circle();
+//        Circle circle2 = new Circle(1.5);
+
+        Scanner input = new Scanner(System.in);
+        double inputRadius = input.nextDouble();
+
+        Circle circle3 = new Circle(inputRadius);
+
+
     }
 }
