285764aed768d289e7ba827399bfc461d5ffa0d0
Heather01 <heather.murray.2019@uni.strath.ac.uk>
2020-10-15 00:58:53 +0100

    completed inheritance in exercise 2.9

--- src/main/java/Circle/Circle.java
+++ src/main/java/Circle/Circle.java
@@ -15,9 +15,9 @@ public class Circle {
         colour = "red";
     }
 
-    public Circle(double r, double colourNumber) {
+    public Circle(double r, String colour) {
         radius = r;
-        setColour(colourNumber);
+        setColour(colour);
     }
 
     public double getRadius() {
@@ -32,25 +32,8 @@ public class Circle {
         return colour;
     }
 
-    public void setColour(double number) {
-        if(number >= 0 && number<=5) {
-            colour = "red";
-        }
-        else if(number > 5 && number<=10) {
-            colour = "amber";
-        }
-        else if(number > 10 && number<=15) {
-            colour = "blue";
-        }
-        else if(number > 15 && number<=20) {
-            colour = "purple";
-        }
-        else if(number >20  && number<=25) {
-            colour = "yellow";
-        }
-        else if(number >25) {
-            colour = "pink";
-        }
+    public void setColour(String colour) {
+        this.colour = colour;
     }
 
 }
